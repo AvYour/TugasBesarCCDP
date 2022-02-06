39c175df4827aa609fbfd163da0a4c3ab7f60b15
AvYour <83781875+AvYour@users.noreply.github.com>
2022-02-06 22:12:15 +0700

    fix carousel, navbar

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,63 +1,80 @@
+<style>
+img {
+    opacity: 1.0;
+    background: rgba(0, 0, 0, 0.8);
+}
+
+img:hover {
+    opacity: 0.5;
+
+}
+</style>
 <main>
-<!--Carousel-->
- <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/wp6392619-batik-android-wallpapers.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-        <h5>Selamat Datang</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="img/wp6392619-batik-android-wallpapers.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/wp6392619-batik-android-wallpapers.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+    <!--Carousel-->
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" style="background: rgba(0, 0, 0, 0.8);">
+                <img src="img/oogha.com_wallpaper.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block position-absolute top-50 start-30">
+                    <h1 class="display1">Selamat Datang</h1>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item" style="background: rgba(0, 0, 0, 0.8)">
+                <img src=" img/1883221.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" style="background-color: rgba(0, 0, 0, 0.8)">
+                <img src="img/batik-1.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
 </main>
 
-		<div class="content-wrapper">
-			<div class="row">
-				<div class="col-md-12 stretch-card">
-					<div class="card">
-						<div class="card-body">
-							<!--Search-->
-							<div class="container-fluid d-flex flex-row-reverse">
-							 <form action="">
-								<div class="p-1 bg-light rounded border rounded-pill shadow-sm mb-4">
-									<div class="input-group">
-									<input type="search" placeholder="Cari Batik" aria-describedby="button-addon1" class="form-control border-0 bg-light">
-									<div class="input-group-append">
-										<button id="button-addon1" type="submit" class="btn btn-link text-primary"><i class="fa fa-search"></i></button>
-									</div>
-								</div>
-								</div>
-							 </form>
-							</div>
-							<!--Content Card-->
-							<div class="container-fluid d-flex justify-content-center">
-								<?php foreach($data['batik'] as $batik):?>
-								<div class="card border-light mx-3" style="width: 15rem;">
-									<img src="img/wp6392619-batik-android-wallpapers.jpg" class="card-img-top" alt="..." style="height: 200px;">
-										<div class="card-body border-light">
-											<h5 class="card-title"><?=$batik['nama'] ?></h5>
-											<a href="<?= BASEURL ?>/home/detail/<?= $batik['id'] ?>" class="btn btn-primary">Baca Sekarang »</a>
-										</div>
-								</div>
-								<?php endforeach;?>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+<div class="content-wrapper">
+    <div class="row">
+        <div class="col-md-12 stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <!--Search-->
+                    <div class="container-fluid d-flex flex-row-reverse">
+                        <form action="">
+                            <div class="p-1 bg-light rounded border rounded-pill shadow-sm mb-4">
+                                <div class="input-group">
+                                    <input type="search" placeholder="Cari Batik" aria-describedby="button-addon1"
+                                        class="form-control border-0 bg-light">
+                                    <div class="input-group-append">
+                                        <button id="button-addon1" type="submit" class="btn btn-link text-primary"><i
+                                                class="fa fa-search"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!--Content Card-->
+                    <div class="container-fluid d-flex justify-content-center">
+                        <?php foreach($data['batik'] as $batik):?>
+                        <div class="card border-light mx-3" style="width: 15rem;">
+                            <img src="img/wp6392619-batik-android-wallpapers.jpg" class="card-img-top" alt="..."
+                                style="height: 200px;">
+                            <div class="card-body border-light">
+                                <h5 class="card-title"><?=$batik['nama'] ?></h5>
+                                <a href="<?= BASEURL ?>/home/detail/<?= $batik['id'] ?>" class="btn btn-primary">Baca
+                                    Sekarang »</a>
+                            </div>
+                        </div>
+                        <?php endforeach;?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,36 +8,37 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    		<!-- plugins:css -->
-		<link rel="stylesheet" href="<?= BASEURL ?>/vendors/mdi/css/materialdesignicons.min.css" />
-		<link rel="stylesheet" href="<?= BASEURL ?>/vendors/base/vendor.bundle.base.css" />
-		<!-- endinject -->
-		<!-- plugin css for this page -->
-		<link rel="stylesheet" href="<?= BASEURL ?>/vendors/datatables.net-bs4/dataTables.bootstrap4.css" />
-		<!-- End plugin css for this page -->
-		<!-- inject:css -->
-		<link rel="stylesheet" href="<?= BASEURL ?>/css/style.css" />
-		<!--Plugin icon-->
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-		<!-- endinject -->
-		<!-- JQuery -->
-		<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
-		<!-- SweetAlert2 -->
-		<script src=" //cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <title>Halaman <?=$data['judul'] ?></title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="<?= BASEURL ?>/vendors/mdi/css/materialdesignicons.min.css" />
+    <link rel="stylesheet" href="<?= BASEURL ?>/vendors/base/vendor.bundle.base.css" />
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="<?= BASEURL ?>/vendors/datatables.net-bs4/dataTables.bootstrap4.css" />
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="<?= BASEURL ?>/css/style.css" />
+    <!--Plugin icon-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <!-- endinject -->
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
+    <!-- SweetAlert2 -->
+    <script src=" //cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <title>Halaman <?=$data['judul'] ?></title>
 </head>
 
 <body>
-		<!--Navbar-->
-		<header>
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<div class="container-fluid">
-					<a class="navbar-brand" href="<?= BASEURL ?>">Batik</a>
-					<div class="d-flex">
-						<a class="btn btn-outline-success" href="<?= BASEURL ?>/login">Login</a>
-						<a class="btn btn-outline-success" href="<?= BASEURL ?>/login/register">Daftar</a>
-					</div>
-				</div>
-			</nav>
-		</header>
+    <!--Navbar-->
+    <header>
+        <nav class="navbar navbar-light" style="background-color: #FF9F45;">
+            <div class="container-fluid">
+                <a class="navbar-brand text-light" href="<?= BASEURL ?>">Batik</a>
+                <div class="d-flex">
+                    <a class="btn btn-outline-success" href="<?= BASEURL ?>/login">Login</a>
+                    <a class="btn btn-outline-success" href="<?= BASEURL ?>/login/register">Daftar</a>
+                </div>
+            </div>
+        </nav>
+    </header>
