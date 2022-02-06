4e4b4a3a7ab60536c06cd98359c6e9e2559f0fb0
Faisal O <klsfaisalobara@gmail.com>
2022-02-06 22:21:35 +0700

    batik

--- app/views/dashboard/batik.php
+++ app/views/dashboard/batik.php
@@ -9,7 +9,20 @@
             <div class="d-flex justify-content-between flex-wrap">
                 <div class="d-flex align-items-end flex-wrap">
                     <div class="me-md-3 me-xl-5">
-                        <h2>Halaman Batik</h2>
+                        <h1>Batik</h1>
+                    </div>
+                    <!--Content Card-->
+                    <!-- Ganti pake database gan + pagination -->
+                    <div class="d-flex flex-wrap justify-content-center">
+                        <?php for ($i = 0; $i < 8; $i++) { ?>
+                            <div class="card border-light mx-3" style="width: 13rem;">
+                                <img src="<?= BASEURL ?>/img/wp6392619-batik-android-wallpapers.jpg" class="card-img-top" alt="..." style="height: 200px;">
+                                <div class="card-body border-light">
+                                    <h5 class="card-title">Title</h5>
+                                    <a href="<?= BASEURL ?>/home/detail/<?= $batik['id'] ?>" class="btn btn-primary">Baca Sekarang »</a>
+                                </div>
+                            </div>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
