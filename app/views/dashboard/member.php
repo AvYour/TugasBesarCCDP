31d059d6165fdcc30804ba3a2b959e6db1bf1d81
rifkikautsar <rifkikautsar1@gmail.com>
2022-02-07 16:42:03 +0700

    fix jquery datatable

--- app/views/dashboard/member.php
+++ app/views/dashboard/member.php
@@ -5,46 +5,47 @@
             <div class="col-6">
                 <?=Flasher::flash(); ?>
             </div>
-            <table class="table table-striped mt-4 table-paginate" align="center">
-                <thead>
-                    <tr class="table-dark" align="center">
-                        <td>Nama</td>
-                        <td>Email</td>
-                        <td>Hak Akses</td>
-                        <td>Tanggal Lahir</td>
-                        <td>JK</td>
-                        <td>Aksi </td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($data['member'] as $data) :?>
-                    <tr align="center">
-                        <td><?= $data['nama'] ?></td>
-                        <td><?= $data['email'] ?></td>
-                        <td><?= $data['hakAkses'] ?></td>
-                        <td><?= $data['tgl_lahir'] ?></td>
-                        <td><?= $data['jk'] ?></td>
-                        <td>
-                            <button type="button" id="<?= BASEURL ?>/dashboard/hapusUser/<?=$data['id']?>"
-                                class="btn btn-danger hapus">Hapus</button>
-                        </td>
-                    </tr>
-                    <!-- Ganti pake database gan + pagination -->
-                </tbody>
-                <?php endforeach; ?>
-            </table>
+            <div class="table-responsive">
+                <table id="data-member" class="table table-striped mt-4 table-paginate" align="center">
+                    <thead>
+                        <tr class="table-dark" align="center">
+                            <td>Nama</td>
+                            <td>Email</td>
+                            <td>Hak Akses</td>
+                            <td>Tanggal Lahir</td>
+                            <td>JK</td>
+                            <td>Aksi </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data['member'] as $data) :?>
+                        <tr align="center">
+                            <td><?= $data['nama'] ?></td>
+                            <td><?= $data['email'] ?></td>
+                            <td><?= $data['hakAkses'] ?></td>
+                            <td><?= $data['tgl_lahir'] ?></td>
+                            <td><?= $data['jk'] ?></td>
+                            <td>
+                                <button type="button" id="<?= BASEURL ?>/dashboard/hapusUser/<?=$data['id']?>"
+                                    class="btn btn-danger hapus">Hapus</button>
+                            </td>
+                        </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
 <!-- content-wrapper ends -->
 <script>
 $(document).ready(function() {
-    $('.table-paginate').dataTable({
-        "language": {
+    $("#data-member").DataTable({
+        language: {
             "zeroRecords": "Tidak ada data yang ditampilkan",
         }
-    });
-    $(".table-paginate").on("click", ".hapus", function() {
+    })
+    $("#data-member").on("click", ".hapus", function() {
         let url = $(this).attr("id");
         console.log(url);
         Swal.fire({
