c7df2ca9bfb09007b71f8c742dff398cde337cc6
AvYour <83781875+AvYour@users.noreply.github.com>
2022-02-07 18:13:51 +0700

    please add modal

--- app/views/home/detail.php
+++ app/views/home/detail.php
@@ -14,36 +14,9 @@
                     <div class="container-fluid d-flex justify-content-end mb-2">
                         <div class="row">
                             <div class="col">
-                                <button type="button" class="btn btn-primary" data-toggle="modal"
-                                    data-target="#exampleModalLong">
-                                    Launch demo modal
-                                </button>
-
-                                <!-- Modal -->
-                                <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog"
-                                    aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                                                <button type="button" class="close" data-dismiss="modal"
-                                                    aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                ...
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
-                                                    data-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary">Save changes</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <button class="btn btn-primary rounded-pill" style="font-size: 12pt;"
+                                    type="button">Edit</button>
                             </div>
-
                             <div class="col">
                                 <button class="btn btn-danger rounded-pill" style="font-size: 12pt;"
                                     type="button">Hapus</button>
