f2158a33c5ac2e29a5646a5e417f116985bda611
Faisal O <klsfaisalobara@gmail.com>
2022-02-07 13:19:14 +0700

    fix tampilan batik

--- app/views/dashboard/batik.php
+++ app/views/dashboard/batik.php
@@ -4,32 +4,38 @@
             <?php Flasher::flash(); ?>
         </div>
     </div>
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-md-12 grid-margin">
-            <div class="d-flex justify-content-between flex-wrap">
+            <div class="d-flex justify-content flex-wrap">
                 <div class="d-flex align-items flex-wrap">
-                    <div class="me-md-3 me-xl-5 mb-5">
+                    <div class="me-md-3 me-xl-5">
                         <h1>Batik</h1>
-                    </div>
-                    <!--Content Card-->
-                    <!-- Ganti pake database gan + pagination -->
-                    <div class="d-flex flex-wrap justify-content-center">
-                        <?php foreach($data['batik'] as $batik): ?>
-                        <div class="card border-light mx-3" style="width: 13rem;">
-                            <img src="<?= BASEURL ?>/img/wp6392619-batik-android-wallpapers.jpg" class="card-img-top"
-                                alt="..." style="height: 200px;">
-                            <div class="card-body border-light">
-                                <h5 class="card-title">Title</h5>
-                                <a href="<?= BASEURL ?>/dashboard/detail/<?= $batik['id'] ?>"
-                                    class="btn btn-primary">Baca
-                                    Sekarang »</a>
-                            </div>
-                        </div>
-                        <?php endforeach ?>
                     </div>
                 </div>
             </div>
         </div>
+    </div> -->
+
+    <div class="row">
+        <h1>Batik</h1>
     </div>
+
+    <!--Content Card-->
+    <!-- Ganti pake database gan + pagination -->
+    <div class="row">
+        <div class="d-flex flex-wrap justify-content-between">
+            <?php foreach ($data['batik'] as $batik) : ?>
+                <div class="card border-light mx-3 mt-3" style="width: 13rem;">
+                    <img src="<?= BASEURL ?>/img/wp6392619-batik-android-wallpapers.jpg" class="card-img-top" alt="..." style="height: 200px;">
+                    <div class="card-body border-light">
+                        <h5 class="card-title"><?= $batik['nama'] ?></h5>
+                        <a href="<?= BASEURL ?>/dashboard/detail/<?= $batik['id'] ?>" class="btn btn-primary">Baca
+                            Sekarang »</a>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+
 </div>
 <!-- content-wrapper ends -->
