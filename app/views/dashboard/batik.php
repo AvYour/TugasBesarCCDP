437d35bf32bc9a25d44a4ea1d43bbb1e258d7e0f
Faisal O <klsfaisalobara@gmail.com>
2022-02-07 17:44:44 +0700

    add dashboard

--- app/views/dashboard/batik.php
+++ app/views/dashboard/batik.php
@@ -9,26 +9,24 @@
             <div class="d-flex justify-content flex-wrap">
                 <div class="d-flex align-items flex-wrap">
                     <div class="container-fluid">
-                        <div class="me-md-3 me-xl-5 mb-5">
+                        <div class="me-md-3 me-xl-5">
                             <h1>Batik</h1>
                         </div>
                     </div>
                     <!--Content Card-->
                     <!-- Ganti pake database gan + pagination -->
                     <div class="d-flex flex-wrap justify-content-center">
-                        <?php foreach($data['batik'] as $batik): ?>
-                        <div class="card border-light mx-3" style="width: 20rem;">
-                            <img src="<?= BASEURL ?>/img/wp6392619-batik-android-wallpapers.jpg" class="card-img-top"
-                                alt="..." style="height: 200px;">
-                            <div class="card-body border-light">
-                                <h5 class="card-title">Title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
-                                <a href="<?= BASEURL ?>/dashboard/detail/<?= $batik['id'] ?>"
-                                    class="btn btn-primary">Baca
-                                    Sekarang »</a>
+                        <?php foreach ($data['batik'] as $batik) : ?>
+                            <div class="card border-light mx-3 mt-4" style="width: 17rem;">
+                                <img src="<?= BASEURL ?>/img/wp6392619-batik-android-wallpapers.jpg" class="card-img-top" alt="..." style="height: 200px;">
+                                <div class="card-body border-light">
+                                    <h5 class="card-title"><?= $batik['nama'] ?></h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.</p>
+                                    <a href="<?= BASEURL ?>/dashboard/detail/<?= $batik['id'] ?>" class="btn btn-primary">Baca
+                                        Sekarang »</a>
+                                </div>
                             </div>
-                        </div>
                         <?php endforeach ?>
                     </div>
                 </div>
--- app/views/dashboard/member.php
+++ app/views/dashboard/member.php
@@ -3,7 +3,7 @@
         <div class="me-md-3 me-xl-5">
             <h1>Member</h1>
             <div class="col-6">
-                <?=Flasher::flash(); ?>
+                <?= Flasher::flash(); ?>
             </div>
             <table class="table table-striped mt-4 table-paginate" align="center">
                 <thead>
@@ -17,49 +17,48 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($data['member'] as $data) :?>
-                    <tr align="center">
-                        <td><?= $data['nama'] ?></td>
-                        <td><?= $data['email'] ?></td>
-                        <td><?= $data['hakAkses'] ?></td>
-                        <td><?= $data['tgl_lahir'] ?></td>
-                        <td><?= $data['jk'] ?></td>
-                        <td>
-                            <button type="button" id="<?= BASEURL ?>/dashboard/hapusUser/<?=$data['id']?>"
-                                class="btn btn-danger hapus">Hapus</button>
-                        </td>
-                    </tr>
-                    <!-- Ganti pake database gan + pagination -->
+                    <?php foreach ($data['member'] as $data) : ?>
+                        <tr align="center">
+                            <td><?= $data['nama'] ?></td>
+                            <td><?= $data['email'] ?></td>
+                            <td><?= $data['hakAkses'] ?></td>
+                            <td><?= $data['tgl_lahir'] ?></td>
+                            <td><?= $data['jk'] ?></td>
+                            <td>
+                                <button type="button" id="<?= BASEURL ?>/dashboard/hapusUser/<?= $data['id'] ?>" class="btn btn-danger hapus">Hapus</button>
+                            </td>
+                        </tr>
+                        <!-- Ganti pake database gan + pagination -->
                 </tbody>
-                <?php endforeach; ?>
+            <?php endforeach; ?>
             </table>
         </div>
     </div>
 </div>
 <!-- content-wrapper ends -->
 <script>
-$(document).ready(function() {
-    $('.table-paginate').dataTable({
-        "language": {
-            "zeroRecords": "Tidak ada data yang ditampilkan",
-        }
-    });
-    $(".table-paginate").on("click", ".hapus", function() {
-        let url = $(this).attr("id");
-        console.log(url);
-        Swal.fire({
-            title: 'Apakah anda yakin menghapus data user?',
-            icon: 'warning',
-            allowOutsideClick: false,
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Hapus!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                document.location.href = url;
+    $(document).ready(function() {
+        $('.table-paginate').dataTable({
+            "language": {
+                "zeroRecords": "Tidak ada data yang ditampilkan",
             }
+        });
+        $(".table-paginate").on("click", ".hapus", function() {
+            let url = $(this).attr("id");
+            console.log(url);
+            Swal.fire({
+                title: 'Apakah anda yakin menghapus data user?',
+                icon: 'warning',
+                allowOutsideClick: false,
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Hapus!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.location.href = url;
+                }
+            })
         })
     })
-})
 </script>
