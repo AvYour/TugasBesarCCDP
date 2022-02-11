86c1211d95915b77da3c6b760f489b2f3c8cbddb
rifkikautsar <rifkikautsar1@gmail.com>
2022-02-11 20:17:15 +0700

    remove edit button in landing page

--- app/views/home/detail.php
+++ app/views/home/detail.php
@@ -11,47 +11,7 @@
                             Kembali
                         </a>
                     </div>
-                    <div class="container-fluid d-flex justify-content-end mb-2">
-                        <div class="row">
-                            <div class="col">
-                                <button class="btn btn-primary rounded-pill" style="font-size: 12pt;" type="button"
-                                    data-bs-toggle="modal" data-bs-target="#staticBackdrop">Edit</button>
-                            </div>
-                            <!-- <div class="col">
-                                <button class="btn btn-danger rounded-pill" style="font-size: 12pt;"
-                                    type="button">Hapus</button>
-                            </div> -->
-                        </div>
-                    </div>
-                    <!-- Modal Edit -->
-                    <!-- Button trigger modal -->
-                    <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#staticBackdrop">
-                        Launch static backdrop modal
-                    </button> -->
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
-                        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary"
-                                        data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Understood</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Modal -->
                     <div class="container-fluid d-flex justify-content-center">
                         <img src="http://localhost/rpl2/public/img/wp6392619-batik-android-wallpapers.jpg"
                             class="img-fluid me- rounded" alt="..." style="height: 350px; width:500px">
